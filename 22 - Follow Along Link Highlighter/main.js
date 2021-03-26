72fed259339f10d234d4915667f1df29dbbf8cf4
Md. Imam Hasan <imamhasan0007@gmail.com>
2021-03-26 10:17:46 +0600

    22th day: Follow Along Link Highlighter complete

---
+++ 22 - Follow Along Link Highlighter/main.js
@@ -0,0 +1,21 @@
+const trigger = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink(){
+  const linkCords = this.getBoundingClientRect();
+  console.log(linkCords);
+  const cords = {
+    width: linkCords.width,
+    height: linkCords.height,
+    top: linkCords.top+window.scrollY,
+    left: linkCords.left+window.scrollX
+  }
+
+  highlight.style.width = `${cords.width}px`;
+  highlight.style.height = `${cords.height}px`;
+  highlight.style.transform = `translate(${cords.left}px,${cords.top}px)`;
+}
+
+trigger.forEach(a=>a.addEventListener('mouseenter',highlightLink));
